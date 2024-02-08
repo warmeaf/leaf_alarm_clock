0fffb6a666994d836b43b8527ba483bc13720058
warmleaf <1391040917@qq.com>
2024-02-08 09:29:53 +0800

    优化样式

--- lib/main.dart
+++ lib/main.dart
@@ -7,23 +7,31 @@
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-
 import 'app/routes/app_pages.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  initSystem();
+
   runApp(
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: "叶子闹钟",
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.lightGreen,
-        ),
+            seedColor: Colors.lightGreen, background: Colors.lightGreen[50]),
       ),
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
     ),
   );
+}
+
+initSystem() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.lightGreen[50], // 设置状态栏的背景颜色为透明色
+    statusBarBrightness: Brightness.dark, // 设置状态栏文字为暗色
+    statusBarIconBrightness: Brightness.dark, // 设置状态栏图标为暗色
+  ));
 }
